d4d0ea52dedb1a7b548f167276d1413c819c5879
Joao Zsigmond <jzsiggy@gmail.com>
2019-04-12 15:14:12 -0300

    added js, css and index.html

--- flaskServ.py
+++ flaskServ.py
@@ -9,30 +9,34 @@ mongo = PyMongo(app)
 
 '''class Cam:
 	def __init__(self, status):
-		self.status = '''''
+		self.status = '''
 
 
 @app.route('/')
-def hello_world():
-	return 'Hello, World!'
+def index():
+	return render_template('index.html')
 
 
 @app.route('/cam', methods = ["POST", "GET"])
 def cam():
 	if request.method == 'GET':
-		# data = dumps(mongo.db.camdb.find({'cam1': 'True'}))
+		# data = dumps(mongo.db.camdb.find({"camera0": "True"}))
 		# return data
-		data = dumps(mongo.db.camdb.find())
-		#return data[-17:-2]
-		return render_template('cam.html', cam1_status=data[-17:-2])
+		# return render_template('cam.html', cam0_status=data[-17:-2])
+		# l=[]
+		# for posts in mongo.db.camdb.find():
+		# 	l.append(posts)
+		# data = l[-1]['camera0']
+		data=mongo.db.camdb.find()[mongo.db.camdb.count()-1]["cam0"]
+		return render_template('cam.html', cam0_status=data)
 		
 
 	if request.method == 'POST':
 		req=request.form.to_dict()
 		mongo.db.camdb.insert_one(req)
-		status = req['cam1']
-		return redirect('/cam')
-		# return status
+		status = req['cam0']
+		# return redirect('/cam')
+		return status
 
 
 
